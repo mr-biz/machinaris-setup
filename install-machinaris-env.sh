a62e32bd959b8960df34216c770cfcaeac345322
mr-biz <44796116+mr-biz@users.noreply.github.com>
2021-10-11 00:32:24 +0100

    Update install-machinaris-env.sh

--- install-machinaris-env.sh
+++ install-machinaris-env.sh
@@ -18,13 +18,12 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 #
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 #Install latest docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-nano docker-compose.yml #Copy from  https://www.machinaris.app/ #
 #Manage Docker as a non-root user
 sudo groupadd docker
 sudo usermod -aG docker $USER
@@ -33,7 +32,8 @@ sudo usermod -aG docker $USER
 mkdir ~/machinaris
 cd ~/machinaris
 echo "#Copy and Paste the docker compose data from https://www.machinaris.app/ here.">>docker-compose.yml
-nano nano docker-compose.yml
+nano docker-compose.yml
+echo "After reboot enter the command "docker-compose up" and press enter"
 #
 echo "Rebooting...when the system comes up run "docker-compose up"."
 sleep 10
